4730ff8d1920c82b90eedbeb7ab5ec57ef2e83fc
hsravo <hsravo1@gmail.com>
2021-03-09 16:08:03 +0100

    mailer prod fix ? 1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,11 @@ class HomeController < ApplicationController
   end
 
   def admin
+    if user_signed_in?
+      if current_user.role == 'admin'
+        redirect_to dashboard_path
+      end
+    end
   end
 
   def contact
