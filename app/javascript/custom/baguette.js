049b39309136ba94c9e619cf226506d9715c5e8a
hsravo <hsravo1@gmail.com>
2021-03-11 19:45:52 +0100

    about + contact pages ok

--- app/javascript/custom/baguette.js
+++ app/javascript/custom/baguette.js
@@ -5,3 +5,11 @@ window.addEventListener('load', function() {
 window.addEventListener('load', function() {
     baguetteBox.run('#publication-gallery');
 });
+
+window.addEventListener('load', function() {
+    baguetteBox.run('#contact-img-field');
+});
+
+window.addEventListener('load', function() {
+    baguetteBox.run('#image-field');
+});
