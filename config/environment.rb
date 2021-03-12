50af30f3efd6c5075cc745794dc80f89c058b3fd
hsravo <hsravo1@gmail.com>
2021-03-12 23:37:52 +0100

    change environment+production mailer url

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
-  :domain => 'laura-aubree6.herokuapp.com',
+  :domain => 'laura-aubree.fr',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
