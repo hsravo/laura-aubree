1715a458eba2bdb1c989ffc428480e78e3972d27
hsravo <hsravo1@gmail.com>
2021-04-12 17:38:46 +0200

    production mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'https://www.laura-aubree6.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'laura-aubree6.herokuapp.com/' }
   config.action_mailer.perform_deliveries = true
 
   # Inserts middleware to perform automatic connection switching.
