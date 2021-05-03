6d7c7e3b0763ebdd4e176303a405dca6d71b98b1
hsravo <hsravo1@gmail.com>
2021-05-03 11:20:47 +0200

    user fix

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -7,6 +7,10 @@ module Admin
 
     def index
       @projects = Project.all
+      respond_to do |format|
+        format.html  # index.html.erb
+        format.json  { render :json => @projects }
+      end
     end
 
     def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   attr_accessor :login 
 
   has_many :publications
-  # has_many :projects
+  has_many :projects
 
   validates :username, presence: :true, uniqueness: {case_sensitive: false}, format: {with: /\A[a-zA-Z0-9 _\.]*\z/}
 
